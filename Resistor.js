c5ba6488002f45cb552ba450b095c30176507bdd
AcinonyxLeo <gabriellejohn15@gmail.com>
2022-12-13 20:32:52 +0800

    first

--- Resistor.js
+++ Resistor.js
@@ -1,13 +1,34 @@
-$("div.band , img.picture").hide();
-$("img.picture.banthree").show();
 
 $(document).ready(function(){
     
-    $('select.form-control').on('change', function(){
-        var demovalue = $(this).val();
-        $("div.band , img.picture").hide();
-        $("div.band.ban" + demovalue).show();
-        $("img.picture.ban" + demovalue).show();
+    $('div.band, img.picture').hide();
+    $('img.picture.banthree').show();
+
+    $('#selection').on('change', function(){
+        var bvalue = $(this).val();
+        $('div.band, img.picture').hide();
+        $('div.band.ban' + bvalue).show();
+        $('img.picture.ban' + bvalue).show();
+    });
+
+    function Calculate(first, second, multiplier){
+        resistor = (first+second)*multiplier
+        return resistor         
+    };
+
+    $('#calculate').click(function(){
+
+        var first = $('#1stband').val();
+
+        var second = $('#2ndband').val();
+
+        var multiplier = $('#multi').val();
+
+        testband = Calculate(first,second,multiplier);
+
+        $('#resistorvalue').text(testband);
+
+        console.log(testband);
     });
 
 });
