ae83f025a482a4a9fb52e08ce5705a6dd7b25331
AcinonyxLeo <gabriellejohn15@gmail.com>
2022-12-15 10:59:28 +0800

    second

--- Resistor.js
+++ Resistor.js
@@ -4,17 +4,30 @@ $(document).ready(function(){
     $('div.band, img.picture').hide();
     $('img.picture.ban3').show();
 
+    var demovalue = 0
+
     $('#selection').on('change', function(){
-        var bvalue = $(this).val();
+        demovalue = $(this).val();
         $('div.band, img.picture').hide();
-        $('div.band.ban' + bvalue).show();
-        $('img.picture.ban' + bvalue).show();
+        $('div.band.ban' + demovalue).show();
+        $('img.picture.ban' + demovalue).show();
+
+        $('#1stBand').prop('selectedIndex', 0);
+        $('#2ndBand').prop('selectedIndex', 0);
+        $('#3rdBand').prop('selectedIndex', 0);
+        $('#multi').prop('selectedIndex', 0);
+        $('#tole').prop('selectedIndex', 0);
+        $('#temp').prop('selectedIndex', 0);
     });
 
     function Calculate(first, second, third, multiplier){
+        if (demovalue <=4){
+            resistor = (first+second)*multiplier
+            return resistor   
+        } else {
         resistor = (first+second+third)*multiplier
         return resistor       
-        
+        }
     }
 
     $('#calculate').click(function(){
@@ -23,12 +36,13 @@ $(document).ready(function(){
         var second = $('#2ndBand').val();
         var third = $('#3rdBand').val();
         var multiplier = $('#multi').val();
+        var tolerance = $('#tole').val();
+        var temperature = $('#temp').val();
 
-        testband = Calculate(first,second,third,multiplier);
+        calculated = Calculate(first,second,third,multiplier,tolerance,temperature);
+        $('#resistorvalue').text("Resistor: " + calculated + " ohms");
+        $('#tolerancevalue').text("Tolerance: " + tolerance);
+        $('#temperaturevalue').text("Temperature Coefficient: " + temperature);
 
-        $('#resistorvalue').text(testband);
-
-        console.log(testband);
     });
-
-});
+})
